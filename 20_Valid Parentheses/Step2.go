0b18e807e34a2e7dbf76b4e5d832eb9e770dba37
kawasakiko <honey416x2@gmail.com>
2026-02-22 18:55:01 +0900

    commit

---
+++ 20_Valid Parentheses/Step2.go
@@ -0,0 +1,18 @@
+func isValid(s string) bool {
+	stack := []rune{}
+	hash := map[rune]rune{')': '(', ']': '[', '}': '{'}
+
+	for _, char := range s {
+		if match, found := hash[char]; found {
+			// スタックが空でない、かつスタックの一番上と一致するか確認する
+			if len(stack) > 0 && stack[len(stack)-1] == match {
+				stack = stack[:len(stack)-1] // Pop
+			} else {
+				return false
+			}
+		} else {
+			stack = append(stack, char)
+		}
+	}
+	return len(stack) == 0
+}
